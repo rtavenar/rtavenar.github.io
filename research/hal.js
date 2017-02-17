ececc53623c95dc62499cebe0cbb70ebe6232a52
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-02-17 17:50:51 +0100

    research website html/css+js

--- research/hal.js
+++ research/hal.js
@@ -14,6 +14,7 @@ function GetAjaxObject() {
 var blacklist=[519490, 662905, 1254455];
 var idhal='rtavenar';
 var num_paper=0;
+var header_pub = "<p>This publication list is generated automatically from my <a href=\"https://halshs.archives-ouvertes.fr/index/index\">HAL</a> record. You can also have a look at my <a href=\"http://scholar.google.com/citations?user=wn1XFWMAAAAJ&amp;hl=fr\">Google Scholar Profile</a> if you prefer.</p>";
 var month=new Array('','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec');
 var countries={'AF':'Afghanistan','AX':'Aland Islands','AL':'Albania','DZ':'Algeria','AS':'American Samoa','AD':'Andorra','AO':'Angola','AI':'Anguilla','AQ':'Antarctica','AG':'Antigua And Barbuda','AR':'Argentina','AM':'Armenia','AW':'Aruba','AU':'Australia','AT':'Austria','AZ':'Azerbaijan','BS':'Bahamas','BH':'Bahrain','BD':'Bangladesh','BB':'Barbados','BY':'Belarus','BE':'Belgium','BZ':'Belize','BJ':'Benin','BM':'Bermuda','BT':'Bhutan','BO':'Bolivia','BA':'Bosnia And Herzegovina','BW':'Botswana','BV':'Bouvet Island','BR':'Brazil','IO':'British Indian Ocean Territory','BN':'Brunei Darussalam','BG':'Bulgaria','BF':'Burkina Faso','BI':'Burundi','KH':'Cambodia','CM':'Cameroon','CA':'Canada','CV':'Cape Verde','KY':'Cayman Islands','CF':'Central African Republic','TD':'Chad','CL':'Chile','CN':'China','CX':'Christmas Island','CC':'Cocos (Keeling) Islands','CO':'Colombia','KM':'Comoros','CG':'Congo','CD':'Congo, Democratic Republic','CK':'Cook Islands','CR':'Costa Rica','CI':'Cote D\'Ivoire','HR':'Croatia','CU':'Cuba','CY':'Cyprus','CZ':'Czech Republic','DK':'Denmark','DJ':'Djibouti','DM':'Dominica','DO':'Dominican Republic','EC':'Ecuador','EG':'Egypt','SV':'El Salvador','GQ':'Equatorial Guinea','ER':'Eritrea','EE':'Estonia','ET':'Ethiopia','FK':'Falkland Islands (Malvinas)','FO':'Faroe Islands','FJ':'Fiji','FI':'Finland','FR':'France','GF':'French Guiana','PF':'French Polynesia','TF':'French Southern Territories','GA':'Gabon','GM':'Gambia','GE':'Georgia','DE':'Germany','GH':'Ghana','GI':'Gibraltar','GR':'Greece','GL':'Greenland','GD':'Grenada','GP':'Guadeloupe','GU':'Guam','GT':'Guatemala','GG':'Guernsey','GN':'Guinea','GW':'Guinea-Bissau','GY':'Guyana','HT':'Haiti','HM':'Heard Island & Mcdonald Islands','VA':'Holy See (Vatican City State)','HN':'Honduras','HK':'Hong Kong','HU':'Hungary','IS':'Iceland','IN':'India','ID':'Indonesia','IR':'Iran, Islamic Republic Of','IQ':'Iraq','IE':'Ireland','IM':'Isle Of Man','IL':'Israel','IT':'Italy','JM':'Jamaica','JP':'Japan','JE':'Jersey','JO':'Jordan','KZ':'Kazakhstan','KE':'Kenya','KI':'Kiribati','KR':'Korea','KW':'Kuwait','KG':'Kyrgyzstan','LA':'Lao People\'s Democratic Republic','LV':'Latvia','LB':'Lebanon','LS':'Lesotho','LR':'Liberia','LY':'Libyan Arab Jamahiriya','LI':'Liechtenstein','LT':'Lithuania','LU':'Luxembourg','MO':'Macao','MK':'Macedonia','MG':'Madagascar','MW':'Malawi','MY':'Malaysia','MV':'Maldives','ML':'Mali','MT':'Malta','MH':'Marshall Islands','MQ':'Martinique','MR':'Mauritania','MU':'Mauritius','YT':'Mayotte','MX':'Mexico','FM':'Micronesia, Federated States Of','MD':'Moldova','MC':'Monaco','MN':'Mongolia','ME':'Montenegro','MS':'Montserrat','MA':'Morocco','MZ':'Mozambique','MM':'Myanmar','NA':'Namibia','NR':'Nauru','NP':'Nepal','NL':'The Netherlands','AN':'Netherlands Antilles','NC':'New Caledonia','NZ':'New Zealand','NI':'Nicaragua','NE':'Niger','NG':'Nigeria','NU':'Niue','NF':'Norfolk Island','MP':'Northern Mariana Islands','NO':'Norway','OM':'Oman','PK':'Pakistan','PW':'Palau','PS':'Palestinian Territory, Occupied','PA':'Panama','PG':'Papua New Guinea','PY':'Paraguay','PE':'Peru','PH':'Philippines','PN':'Pitcairn','PL':'Poland','PT':'Portugal','PR':'Puerto Rico','QA':'Qatar','RE':'Reunion','RO':'Romania','RU':'Russian Federation','RW':'Rwanda','BL':'Saint Barthelemy','SH':'Saint Helena','KN':'Saint Kitts And Nevis','LC':'Saint Lucia','MF':'Saint Martin','PM':'Saint Pierre And Miquelon','VC':'Saint Vincent And Grenadines','WS':'Samoa','SM':'San Marino','ST':'Sao Tome And Principe','SA':'Saudi Arabia','SN':'Senegal','RS':'Serbia','SC':'Seychelles','SL':'Sierra Leone','SG':'Singapore','SK':'Slovakia','SI':'Slovenia','SB':'Solomon Islands','SO':'Somalia','ZA':'South Africa','GS':'South Georgia And Sandwich Isl.','ES':'Spain','LK':'Sri Lanka','SD':'Sudan','SR':'Suriname','SJ':'Svalbard And Jan Mayen','SZ':'Swaziland','SE':'Sweden','CH':'Switzerland','SY':'Syrian Arab Republic','TW':'Taiwan','TJ':'Tajikistan','TZ':'Tanzania','TH':'Thailand','TL':'Timor-Leste','TG':'Togo','TK':'Tokelau','TO':'Tonga','TT':'Trinidad And Tobago','TN':'Tunisia','TR':'Turkey','TM':'Turkmenistan','TC':'Turks And Caicos Islands','TV':'Tuvalu','UG':'Uganda','UA':'Ukraine','AE':'United Arab Emirates','GB':'United Kingdom','US':'United States','UM':'United States Outlying Islands','UY':'Uruguay','UZ':'Uzbekistan','VU':'Vanuatu','VE':'Venezuela','VN':'Viet Nam','VG':'Virgin Islands, British','VI':'Virgin Islands, U.S.','WF':'Wallis And Futuna','EH':'Western Sahara','YE':'Yemen','ZM':'Zambia','ZW':'Zimbabwe'};
 var publishers={'Institute of Electrical and Electronics Engineers':'IEEE','Springer Verlag (Germany)':'Springer','Association for Computing Machinery (ACM)':'ACM'}
@@ -209,7 +210,7 @@ var hals={
         num_paper = num_paper + 1;
         return s;
     }
-    function fetchPublications(target_id,group_key,docids){
+    function fetchPublications(target_id,group_key,docids,header){
         var xmlhttp=GetAjaxObject();
         xmlhttp.onreadystatechange=function(){
             if(xmlhttp.readyState==4){
@@ -217,9 +218,9 @@ var hals={
                     hals[target_id]=hals[target_id].concat(JSON.parse(xmlhttp.responseText).response.docs);
                     if (document.getElementById(target_id) != null) {
                         if(group_key!=''){
-                            document.getElementById(target_id).innerHTML=formatPublicationsByGroup(hals[target_id],group_key);
+                            document.getElementById(target_id).innerHTML=formatPublicationsByGroup(hals[target_id],group_key,header);
                         }else{
-                            document.getElementById(target_id).innerHTML=formatPublicationGroup(hals[target_id],group_key);
+                            document.getElementById(target_id).innerHTML=formatPublicationGroup(hals[target_id],group_key,header);
                         }
                     }
                 }else{
@@ -242,16 +243,16 @@ var hals={
     function ifndef(x,val){
         return(typeof x=='undefined')?val:x;
     }
-    function formatPublicationGroup(docs,group_key){
-        var s='';
+    function formatPublicationGroup(docs,group_key,header){
+        var s=header;
         docs.sort(function(x,y){return 14*(y.producedDateY_i-x.producedDateY_i)+ifndef(y.producedDateM_i,13)-ifndef(x.producedDateM_i,13);});
         for(var j=0;j<docs.length;++j){
             s+=formatDoc(docs[j],group_key);
         }
         return s;
     }
-    function formatPublicationsByGroup(docs,group_key){
-        var s='';
+    function formatPublicationsByGroup(docs,group_key,header){
+        var s=header;
         var doc_by_group={};
         var groups=new Array();
         for(var i=0;i<docs.length;++i){
@@ -273,17 +274,17 @@ var hals={
             var docs=doc_by_group[group];
             if(typeof docs!='undefined'&&docs.length>0){
                 s+='<h2>'+clean(group,categories);
-                s+='</h2>';s+=formatPublicationGroup(docs,group_key);
+                s+='</h2>';s+=formatPublicationGroup(docs,group_key,'');
             }
         }
         return s;
     }
-    function reformatPublicationByGroup(target_id,group_key){
-        document.getElementById(target_id).innerHTML=formatPublicationsByGroup(hals[target_id],group_key);
+    function reformatPublicationByGroup(target_id,group_key,header){
+        document.getElementById(target_id).innerHTML=formatPublicationsByGroup(hals[target_id],group_key,header);
     };
-    jQuery(document).ready(function(){fetchPublications("publications","producedDateY_i",[]);});
-    jQuery(document).ready(function(){fetchPublications("ml_env","",[1343211, 1228397, 906292]);});
-    jQuery(document).ready(function(){fetchPublications("ml_ts","",[1339007,1357973,1321327,916970]);});
-    jQuery(document).ready(function(){fetchPublications("topic_models","",[872048, 906292]);});
-    jQuery(document).ready(function(){fetchPublications("ir","",[862176,567877,672897,639225,566883,601242,561797]);});
-    jQuery(document).ready(function(){fetchPublications("smartenv","",[1138500,1138508,1138512]);});
+    jQuery(document).ready(function(){fetchPublications("publications","producedDateY_i",[],header_pub);});
+    jQuery(document).ready(function(){fetchPublications("ml_env","",[1343211, 1228397, 906292],"");});
+    jQuery(document).ready(function(){fetchPublications("ml_ts","",[1339007,1357973,1321327,916970],"");});
+    jQuery(document).ready(function(){fetchPublications("topic_models","",[872048, 906292],"");});
+    jQuery(document).ready(function(){fetchPublications("ir","",[862176,567877,672897,639225,566883,601242,561797],"");});
+    jQuery(document).ready(function(){fetchPublications("smartenv","",[1138500,1138508,1138512],"");});

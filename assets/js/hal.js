d1a6b0f69abaaf72689a651e1b43a89d7e2a37b8
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2026-03-30 17:49:47 +0200

    minor

--- assets/js/hal.js
+++ assets/js/hal.js
@@ -271,7 +271,7 @@ var hals={
         document.getElementById(target_id).innerHTML=formatPublicationsByGroup(hals[target_id],group_key,header);
     };
     jQuery(document).ready(function(){fetchPublications("publications","producedDateY_i",[],header_pub);});
-    jQuery(document).ready(function(){fetchPublications("ml_ot","",[3932463, 2174309, 2174316, 2174322],"");});
+    jQuery(document).ready(function(){fetchPublications("ml_ot","",[3932463, 2174309, 2174316, 2174322, 4928469, 5345011],"");});
     jQuery(document).ready(function(){fetchPublications("ml_env","",[4023073, 3187591, 2343851, 1515283, 1620604, 1343211, 1228397, 906292],"");});
     jQuery(document).ready(function(){fetchPublications("ml_ts","",[4023073, 3835907, 4003119, 3025671, 3932463, 3187591, 1622094,1561461,1339007,1357973,1321327,912512],"");});
     jQuery(document).ready(function(){fetchPublications("topic_models","",[3187591, 1473556, 906292],"");});
